04fbc96f3db279c240c5daac1f686482fdd4aa8a
Prashant <prashantiiitnag@gmail.com>
2025-05-18 16:20:10 +0530

    Remove .vercelignore file and clean up package.json by removing unused overrides and adjusting resolutions for lightningcss

--- next.config.js
+++ next.config.js
@@ -4,15 +4,12 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  typescript: {
+    typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    optimizeCss: false
-  }
 };
 
 module.exports = nextConfig;
