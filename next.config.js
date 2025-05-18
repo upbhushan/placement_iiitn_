a82f3230b9eac6df78df9a5a9fc86ceae36b6f6e
Prashant <prashantiiitnag@gmail.com>
2025-05-18 16:16:19 +0530

    Update configuration files: enhance next.config.js, modify package.json scripts, and add .vercelignore

--- next.config.js
+++ next.config.js
@@ -4,12 +4,15 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-    typescript: {
+  typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    optimizeCss: false
+  }
 };
 
 module.exports = nextConfig;
